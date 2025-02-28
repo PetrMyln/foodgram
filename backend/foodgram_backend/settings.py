1fa6a93b9637cdbed345a0586c22c4f10a160628
petrmyln <mylnikovpetr@mail.ru>
2025-02-28 15:17:03 +0300

    error

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -86,7 +86,17 @@ DATABASES = {
     }
 }
 
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
+}"""
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
