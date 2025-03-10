a5cf3991964b6213106fdaa9cbbbcc00edffe524
petrmyln <mylnikovpetr@mail.ru>
2025-03-10 12:46:26 +0300

    postman 222 passed, postgres db

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET', 'default_secret_key')
 
-DEBUG = True
+DEBUG = False
 # DEBUG = os.getenv('FOR_DEBAG') == 'False'
 
 # ALLOWED_HOSTS=['127.0.0.1', " http://127.0.0.1:8000/"]
