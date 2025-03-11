2aeb6ecacfd21c0f015b4a70f9d64fa4239dee57
petrmyln <mylnikovpetr@mail.ru>
2025-03-11 14:02:15 +0300

    prepare to add model shortlink

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -210,8 +210,8 @@ class FavoriteRecipe(NameModel):
 
 class ShortLink(models.Model):
     recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE)
-    short_link = models.CharField(max_length=3, unique=True)
-    original_url = models.CharField(max_length=120, unique=True)
+    short_link = models.CharField(max_length=15, unique=True)
+    original_url = models.CharField(max_length=256, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
