820b72cab1fc7286ffe5697ee89d5dd7deb7d052
petrmyln <mylnikovpetr@mail.ru>
2025-03-11 11:40:44 +0300

    favorite fix  favorite_rec__user=self.request.user,tags__slug__in=tags

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-"""DATABASES = {
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
